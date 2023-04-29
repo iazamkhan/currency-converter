a7e061d98d0cbabd7a8873024b9c964d9a551911
Azam <azamkhan561@gmail.com>
2023-04-29 07:38:18 +0530

    set up the project

---
+++ src/components/Converter/Converter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Converter.css'
+
+
+function Converter() {
+  return (
+    <div className="container">
+        <h1 className="top-heading">Currency Converter</h1>
+    </div>
+  )
+}
+
+export default Converter
